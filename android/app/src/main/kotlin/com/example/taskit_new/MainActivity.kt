ee6a02f64a990e6238ffba6d875b3e0a45528f2b
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-06-19 14:16:53 -0700

    Pushing To Do App to Github

---
+++ android/app/src/main/kotlin/com/example/taskit_new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.taskit_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TaskIt',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  String title = '';
+  String description = '';
+  String category = '';
+  String priority = 'Low';
+  bool _isLoading = false;
+
+  // function to simulate saving to Firestore
+  Future<void> _saveToFirestore() async {
+    setState(() {
+      _isLoading = true;
+    });
+    await Future.delayed(const Duration(seconds: 2)); // simulate delay
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TaskIt'),
+      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Title',
+                            prefixIcon: const Icon(Icons.title),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a title';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            title = value ?? '';
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Description',
+                            prefixIcon: const Icon(Icons.description),
+                          ),
+                          onSaved: (value) {
+                            description = value ?? '';
+                          },
+                        ),
+                        DropdownButtonFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Category',
+                            prefixIcon: const Icon(Icons.category),
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              category = value ?? '';
+                            });
+                          },
+                          onSaved: (value) {
+                            category = value ?? '';
+                          },
+                          items: <DropdownMenuItem<String>>[
+                            DropdownMenuItem<String>(
+                              value: 'Work',
+                              child: Text('Work'),
+                            ),
+                            DropdownMenuItem<String>(
+                              value: 'Personal',
+                              child: Text('Personal'),
+                            ),
+                          ],
+                        ),
+                        DropdownButtonFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Priority',
+                            prefixIcon: const Icon(Icons.priority_high),
+                          ),
+                          value: priority,
+                          items: const <DropdownMenuItem<String>>[
+                            DropdownMenuItem<String>(
+                              value: 'Low',
+                              child: Text('Low'),
+                            ),
+                            DropdownMenuItem<String>(
+                              value: 'Medium',
+                              child: Text('Medium'),
+                            ),
+                            DropdownMenuItem<String>(
+                              value: 'High',
+                              child: Text('High'),
+                            ),
+                          ],
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              priority = newValue!;
+                            });
+                          },
+                        ),
+                        ElevatedButton(
+                          child: const Text('Submit'),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState!.save();
+                              await _saveToFirestore();
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Submitted!'),
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+    );
+  }
+}
